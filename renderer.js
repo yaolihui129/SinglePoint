b366a7e275235018b5574cd9b6dfacc39a3cc262
yaolihui129 <yaolihui0506>
2019-12-17 13:06:10 +0800

    完成进程间消息通讯

--- renderer.js
+++ renderer.js
@@ -1,8 +1,9 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const { ipcRenderer} = require('electron')
 
-alert(process.version.node)
+window.addEventListener('DOMContentLoaded',()=>{
+	ipcRenderer.send('message','hello from ipcRenderer')
+	ipcRenderer.on('reply',(event,arg) => {
+		document.getElementById('message').innerHTML = arg
+	})
+})
+
