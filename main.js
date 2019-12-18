ca78d75ecfc63cff6e69c5ccb7f9d12ebfad84d7
yaolihui129 <yaolihui0506>
2019-12-18 13:15:23 +0800

    窗口创建类

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow,ipcMain} = require('electron')
 
-app.on('ready', ()=>{
-	mainWindow = new BrowserWindow({
-	  width: 800,
-	  height: 600,
-	  webPreferences: {
-	    nodeIntegration:true
-	  }
-	})
-	
-	mainWindow.loadFile('./renderer/index.html')
+class AppWindow extends BrowserWindow{
+	constructor(config,fileLocation){
+		const basicConfig={
+			width: 800,
+			height: 600,
+			webPreferences: {
+			  nodeIntegration:true
+			}
+		}
+		// const finalConfig = Object.assign(basicConfig,config)
+		const finalConfig = { ...basicConfig,...config}
+		super(finalConfig)
+		this.loadFile(fileLocation)
+		this.once('reay-to-show',()=>{
+			this.show()
+		})
+	}
+}
 
+app.on('ready', ()=>{
+	mainWindow = new AppWindow({},'./renderer/index.html')
 	ipcMain.on('add-music-window',()=>{
 		  console.log('hello from index page')
-		  addWindow = new BrowserWindow({
+		  addWindow = new AppWindow({
 		    width: 500,
 		    height: 400,
-		    webPreferences: {
-		      nodeIntegration:true
-		    },
 			parent:mainWindow
-		  })
-		  addWindow.loadFile('./renderer/add.html')
-		 
+		  },'./renderer/add.html') 
 	})
-	
 })
 
  
